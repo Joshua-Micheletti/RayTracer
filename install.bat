9c50b14d30b1f67687c272d440121594bbfb897c
Joshua-Micheletti <joshua.e.n.micheletti@gmail.com>
2025-09-02 18:42:53 +0200

    remove useless things

--- install.bat
+++
@@ -1,5 +0,0 @@
-pip install glfw
-pip install pyrr
-pip install pillow
-pip install pyOpengl
-pip install pywavefront
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pip install glfw
-pip install pyrr
-pip install pyOpengl
-pip install pywavefront
